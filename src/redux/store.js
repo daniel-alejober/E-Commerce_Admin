0a3d51626f5689bfcf93892d289a3150375cc7f5
unknown <daniel-alejo94@hotmail.com>
2022-02-20 20:28:28 -0600

    files  redux-axios-.env

---
+++ src/redux/store.js
@@ -0,0 +1,68 @@
+/*
+*Vamos a crear el statdo global, e importamos el archivo donde se encuentran los tados iniciales
+* y al igual que useContext debemos envolver toda la app con el Reduce para que sea global
+! lo vamos a hacer en el archivo de index.js
+*/
+
+/*
+*Asi lo vamos a usar, le agregamos dentro del Provider los datos que querramos que sean persistentes
+*import { Provider } from "react-redux";
+*import { PersistGate } from "redux-persist/integration/react";
+*import { store, persistor } from "./redux/store";
+*<React.StrictMode>
+    <Provider store={store}>
+     <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </Provider>
+*</React.StrictMode>
+ */
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import cartRedux from "./cartRedux";
+import userRedux from "./userRedux";
+
+/*
+ *Vamos a hacer la sesion persistente para que cuando reiniciemos no se pierdan los datos,
+ * debemos instalar redux-persist
+ */
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+/*
+ *combinamos los reducers, asi tanto la sesion como los articulos que esten en el
+ *carrito se van a quedar*/
+const rootReducer = combineReducers({ user: userRedux, cart: cartRedux });
+
+/*
+ *Vamos a indicar que valores queremos que sean persistentes
+ */
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+/*
+ *Aqui estamos exportando los reducer de cada respectivo archivo para que sean colocados globalmente
+  !le vamos a agregar persistedReducer a user para que los datos sean persistentes y no se borren al recargar la pagina
+ */
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export let persistor = persistStore(store);
---
+++ src/redux/userRedux.js
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+/*
+ * tenemos que instalar @reduxjs/toolkit y react-redux,
+ * importamos createSlice() y despues lo vamos a inicializar,
+ * dandole un nombre y un estado inicial,
+ * despues en el reducer va a ir las funciones que queremos que se hagan con el estado inicial
+ ! dentro de accion se encuentra el payload que es la data que le pasemos, cuando ya lo
+ ! estemos usando en otros archivos, le pasaremos objetos con nombre y datos como,
+ !correo contraseña
+ */
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    currentUser: null,
+    isFetching: false,
+    error: false,
+    msg: "",
+  },
+  /*
+   *No siempre vamos a necesitar el action ya que no vamos a enviar nada a otros archivos */
+  reducers: {
+    loginStart: (state) => {
+      state.isFetching = true;
+    },
+    loginSuccess: (state, action) => {
+      state.isFetching = false;
+      state.currentUser = action.payload;
+    },
+    loginFailure: (state, action) => {
+      state.isFetching = false;
+      state.error = true;
+      state.msg = action.payload;
+    },
+  },
+});
+
+export const { loginStart, loginSuccess, loginFailure } = userSlice.actions;
+export default userSlice.reducer;
